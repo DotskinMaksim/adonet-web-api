92509e28c250bc571bd9682012cb95819e850328
opilane <opilane@HP-CZC2349HT0>
2024-10-08 13:57:40 +0300

    Add project files.

---
+++ DotskinWebApi/Controllers/PrimitiividController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotskinWebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PrimitiividController : ControllerBase
+    {
+
+        // GET: primitiivid/hello-world
+        [HttpGet("hello-world")]
+        public string HelloWorld()
+        {
+            return "Hello world at " + DateTime.Now;
+        }
+
+        // GET: primitiivid/hello-variable/mari
+        [HttpGet("hello-variable/{nimi}")]
+        public string HelloVariable(string nimi)
+        {
+            return "Hello " + nimi;
+        }
+
+        // GET: primitiivid/add/5/6
+        [HttpGet("add/{nr1}/{nr2}")]
+        public int AddNumbers(int nr1, int nr2)
+        {
+            return nr1 + nr2;
+        }
+
+        // GET: primitiivid/multiply/5/6
+        [HttpGet("multiply/{nr1}/{nr2}")]
+        public int Multiply(int nr1, int nr2)
+        {
+            return nr1 * nr2;
+        }
+
+        // GET: primitiivid/do-logs/5
+        [HttpGet("do-logs/{arv}")]
+        public void DoLogs(int arv)
+        {
+            for (int i = 0; i < arv; i++)
+            {
+                Console.WriteLine("See on logi nr " + i);
+            }
+        }
+        // GET: primitiivid/rand-num/5/10
+        [HttpGet("rand-num/{nr1}/{nr2}")]
+        public int RandNum(int nr1, int nr2)
+        {
+            Random random= new Random();
+            return random.Next(nr1, nr2);
+        }
+        // GET: primitiivid/birthday/2006/01
+        [HttpGet("birthday/{year}/{month}")]
+        public string Birthday(int year, int month)
+        {
+            int years = (month < DateTime.Now.Month) ? DateTime.Now.Year - year : DateTime.Now.Year - year - 1;
+            return "Olete " + years.ToString() + " aastat vana";
+        }
+    }
+}
---
+++ DotskinWebApi/Controllers/ToodeController.cs
@@ -0,0 +1,50 @@
+﻿using DotskinWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotskinWebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ToodeController : ControllerBase
+    {
+        private static Toode _toode = new Toode(1, "Koola", 1.5, true);
+
+        // GET: toode
+        [HttpGet]
+        public Toode GetToode()
+        {
+            return _toode;
+        }
+
+        // GET: toode/suurenda-hinda
+        [HttpGet("suurenda-hinda")]
+        public Toode SuurendaHinda()
+        {
+            _toode.Price = _toode.Price + 1;
+            return _toode;
+        }
+        // GET: toode/change-activity
+        [HttpGet("change-activity")]
+        public Toode ChangeActivity()
+        {
+            _toode.IsActive = !_toode.IsActive;
+            return _toode;
+        }
+
+        // GET: toode/change-name/testname
+        [HttpGet("change-name/{name}")]
+        public Toode ChangeName(string Name)
+        {
+            _toode.Name = Name;
+            return _toode;
+        }
+
+        // GET: toode/multiply-price/3
+        [HttpGet("multiply-price/{num}")]
+        public Toode MultiplyPrice(int Num)
+        {
+            _toode.Price = _toode.Price*Num;
+            return _toode;
+        }
+    }
+}
