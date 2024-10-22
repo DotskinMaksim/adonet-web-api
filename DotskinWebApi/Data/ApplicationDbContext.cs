463126483d95a2f3f5961c33b6e6798265479f5b
opilane <opilane@HP-CZC2349HTZ>
2024-10-22 13:58:08 +0300

    Done Database configuration

---
+++ DotskinWebApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DotskinWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotskinWebApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
--- DotskinWebApi/Program.cs
+++ DotskinWebApi/Program.cs
@@ -1,6 +1,19 @@
+using DotskinWebApi.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContextPool<ApplicationDbContext>(options =>
+    options.UseMySql(connectionString,
+        new MySqlServerVersion(new Version(7, 0, 0))));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
